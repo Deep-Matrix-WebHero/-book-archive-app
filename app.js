c7cc03c545fc5e230886890708ee6d22a012ff85
Md Ataur Rahman <81749481+Deep-Matrix-WebHero@users.noreply.github.com>
2021-09-02 23:22:04 +0600

    js file updated

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-const SearchBooks = () => {
-    searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    console.log(searchText);
-    searchField.value = '';
-
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-        // console.log(url);
-        .then(res => res.json())
-        .then(data => console.log(data));
+const inputField = document.getElementById('input-field');
+const errorHandler = document.getElementById('error');
+const bookContainer = document.getElementById('book-container');
+const searchTotal = document.getElementById('total-search');
 
 
-}
 
-const displaySearchResult = books => {
-    const searchResult = document.getElementById('search-result');
+const searchBook = () => {
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value;
+
+    inputField.value = '';
+    if (inputText === '') {
+        errorHandler.innerText = `No Data Found!please write you bookname!`;
+        errorHandler.style.textAlign = "center";
+        errorHandler.style.color = "red";
+        errorHandler.style.fontWeight = "bold";
+        errorHandler.style.fontSize = "30px";
+
+    } else {
+        errorHandler.innerText = '';
+        const url = `https://openlibrary.org/search.json?q=${inputText}`;
+        fetch(url)
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data => displayBook(data))
+    }
+
+
+
+};
+
+const displayBook = data => {
+    console.log(data);
+    const books = data.docs.slice(0, 29);
+    searchTotal.innerHTML = `<h2 class="text-center text-danger">Total Result:${data.numFound}</h2>`;
+    const bookContainer = document.getElementById('book-container');
+    bookContainer.textContent = '';
+    if (books.length === 0) {
+        errorHandler.innerText = 'data not found!';
+        inputText.style.textAlign = "center";
+        inputText.style.color = "red";
+        inputText.style.fontWeight = "bold";
+        inputText.style.fontSize = "30px";
+        return;
+    } else {
+
+        errorHandler.innerText = '';
+
+    }
     books.forEach(book => {
-        console.log(book);
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.classList.add('col')
         div.innerHTML = `
-        <div class="card">
-                <img src="${}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
+        <div  class="card">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-100 h-100" alt="book image">
+        <div class="card-body">
+            <h5 >Book Name:${book.title}</h5>
+            <h5 >Author Name:${book.author_name}</h5>
+            <h5 >First Publising Year:${book.first_publish_year}</h5>
+            <h5 >Book Publisher:${book.publisher}</h5>
+        </div>
+    </div >
         
         `;
-        searchResult.appendChild('div');
+        bookContainer.appendChild(div);
     })
+
 }
